900846b6fdb8c0dcc8e659492854d738b8625432
unknown <gdonnellyhistory@gmail.com>
2021-05-05 09:55:14 -0400

    baseline stuff. Made models

---
+++ CollabrApi/Models/Profile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollabrApi.Models
+{
+    public class Profile
+    {
+        private long UserId { get; set; }
+        private string AvatarPicturePath { get; set; }
+        private string SongOnePath { get; set; }
+        private string SongTwoPath { get; set; }
+        private string SongThreePath { get; set; }
+        private List<string> Genres { get; set; }
+    }
+}
---
+++ CollabrApi/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollabrApi.Models
+{
+    public class User
+    {
+        private long ID { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        // recommend using artist name vvv
+        private string UserName { get; set; }
+        private string Password { get; set; }
+        private DateTime Birthday { get; set; }
+        private string Email { get; set; }
+        private List<long> Matches { get; set; }
+    }
+}
